1a776a97bab7640ad9e4a2fd791211b9ad6cd835
Hiroshi Miura <miurahr@linux.com>
2017-09-17 18:04:57 +0900

    Travis: fix postgis dependency
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- gdal/ci/travis/cmake/before_install.sh
+++ gdal/ci/travis/cmake/before_install.sh
@@ -4,8 +4,10 @@ set -e
 
 sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
 sudo add-apt-repository -y ppa:miurahr/gta
+# Pin to postgresql provided by ubuntugis.
+echo "Package: *\nPin: release o=apt.postgresql.org,a=trusty-pgdg\nPin-Priority: -1\n" |sudo tee /etc/apt/preferences.d/ubuntugis-unstable-pin.pref @>&1 >/dev/null
 sudo apt-get update
-sudo apt-get install -y postgresql-9.6 postgresql-client-9.6 postgresql-9.6-postgis-2.3 postgresql-9.6-postgis-2.3-scripts libpq-dev
+sudo apt-get install -y postgresql-9.3 postgresql-client-9.3 postgresql-9.3-postgis-2.2 postgresql-9.3-postgis-scripts libgdal20 libpq-dev
 sudo apt-get install -y --allow-unauthenticated python-numpy libpng12-dev libjpeg-dev libgif-dev liblzma-dev libgeos-dev libcurl4-gnutls-dev libproj-dev libxml2-dev libexpat-dev libxerces-c-dev libnetcdf-dev netcdf-bin libpoppler-dev libpoppler-private-dev libspatialite-dev gpsbabel swig libhdf4-alt-dev libhdf5-serial-dev libpodofo-dev poppler-utils libfreexl-dev unixodbc-dev libwebp-dev libepsilon-dev liblcms2-2 libpcre3-dev libcrypto++-dev libdap-dev libfyba-dev libkml-dev libmysqlclient-dev libogdi3.2-dev libcfitsio-dev openjdk-8-jdk couchdb libarmadillo-dev
 sudo apt-get install -y mesa-opencl-icd libclc-dev ocl-icd-opencl-dev
 sudo apt-get install -y librasterlite-dev libgta-dev
