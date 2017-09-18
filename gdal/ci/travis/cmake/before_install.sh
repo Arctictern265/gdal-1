0ff46b696b42ac71a4da71dcb7b3f58d9c613e5d
Hiroshi Miura <miurahr@linux.com>
2017-09-18 17:03:35 +0900

    Travis: improve trusty-cmake
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- gdal/ci/travis/cmake/before_install.sh
+++ gdal/ci/travis/cmake/before_install.sh
@@ -3,24 +3,31 @@
 set -e
 
 sudo add-apt-repository -y ppa:miurahr/gta
+
+# setup postgresql/postgis
 # Pin to postgresql provided by ubuntugis.
 echo "Package: *\nPin: release o=apt.postgresql.org,a=trusty-pgdg\nPin-Priority: -1\n" |sudo tee /etc/apt/preferences.d/ubuntugis-unstable-pin.pref @>&1 >/dev/null
 sudo apt-get update -qq
-
-# setup postgresql/postgis
-sudo apt-get remove postgis libpq5 libpq-dev postgresql-9.2-postgis postgresql-9.2-postgis-2.2-scripts \
- postgresql-9.3-postgis postgresql-9.4-postgis postgresql-9.5-postgis postgresql-9.6-postgis \
- postgresql-9.2 postgresql-9.3 postgresql-9.4 postgresql-9.5 postgresql-9.6 libgdal1h
+sudo apt-get remove libpq-dev libpq5 libgeos-dev libgeos-c1 \
+  postgresql-9.2 postgresql-9.2-postgis-2.3-scripts \
+  postgresql-9.3 postgresql-9.3-postgis-2.3-scripts \
+  postgresql-9.4 postgresql-9.4-postgis-2.3-scripts \
+  postgresql-9.5 postgresql-9.5-postgis-2.3-scripts \
+  postgresql-9.6 postgresql-9.6-postgis-2.3-scripts \
+  postgresql-client postgresql-client-9.2 \
+  postgresql-client-9.3 postgresql-client-9.4 postgresql-client-9.5 postgresql-client-9.6 \
+  postgresql-contrib-9.2 postgresql-contrib-9.3 \
+  postgresql-contrib-9.4 postgresql-contrib-9.5 postgresql-contrib-9.6
 sudo apt-get install -y postgresql-9.3 postgresql-client-9.3 postgresql-9.3-postgis-2.2 postgresql-9.3-postgis-scripts \
  libgdal20 libpq-dev
 
 # dependencies
 sudo apt-get install -y --allow-unauthenticated python-numpy libpng12-dev libjpeg-dev libgif-dev liblzma-dev libgeos-dev \
- libcurl4-gnutls-dev libproj-dev libxml2-dev libexpat-dev libxerces-c-dev libnetcdf-dev netcdf-bin libpoppler-dev \
+ libcurl4-gnutls-dev libproj-dev libxml2-dev libexpat1-dev libxerces-c-dev libnetcdf-dev netcdf-bin libpoppler-dev \
  libpoppler-private-dev libspatialite-dev gpsbabel swig libhdf4-alt-dev libhdf5-serial-dev libpodofo-dev poppler-utils \
  libfreexl-dev unixodbc-dev libwebp-dev libepsilon-dev liblcms2-2 libpcre3-dev libcrypto++-dev libdap-dev libfyba-dev \
  libkml-dev libmysqlclient-dev libogdi3.2-dev libcfitsio3-dev openjdk-8-jdk couchdb libarmadillo-dev \
- mesa-opencl-icd libclc-dev ocl-icd-opencl-dev librasterlite-dev libgta-dev \
+ libclc-dev ocl-icd-opencl-dev librasterlite-dev libgta-dev \
  doxygen texlive-latex-base make cppcheck ccache g++ python-dev pyflakes \
  libsfcgal-dev fossil libgeotiff-dev libcharls-dev libopenjp2-7-dev libcairo2-dev
 
