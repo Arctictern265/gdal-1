c9172001ec4cc9b933d5696c28e183845cf3810e
Hiroshi Miura <miurahr@linux.com>
2017-09-19 06:52:42 +0900

    Travis: update cmake commandline
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- gdal/ci/travis/cmake/install.sh
+++ gdal/ci/travis/cmake/install.sh
@@ -5,21 +5,25 @@ export PATH=$PWD/cmake-3.9.2-Linux-x86_64/bin/:$PATH
 
 mkdir cmake-gdal-debug
 cd cmake-gdal-debug
-LDFLAGS='-lstdc++' cmake ../gdal
+LDFLAGS='-lstdc++' cmake \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_CXX_COMPILER=clang++-4.0 \
+  -DCMAKE_C_COMPILER=clang-4.0 \
+  ../gdal
 
 # XXX: temporary off cppcheck.
 #cd ../gdal && scripts/cppcheck.sh
 
-cmake --target docs >docs_log.txt 2>&1
+cmake --build . --target docs >docs_log.txt 2>&1
 if cat docs_log.txt | grep -i warning | grep -v russian | grep -v brazilian | grep -v setlocale | grep -v 'has become obsolete' | grep -v 'To avoid this warning'; then echo 'Doxygen warnings found' && cat docs_log.txt && /bin/false; else echo 'No Doxygen warnings found'; fi
-cmake --target man >man_log.txt 2>&1
+cmake --build . --target man >man_log.txt 2>&1
 if cat man_log.txt | grep -i warning | grep -v setlocale | grep -v 'has become obsolete' | grep -v 'To avoid this warning'; then echo 'Doxygen warnings found' && cat docs_log.txt && /bin/false; else echo 'No Doxygen warnings found'; fi
-cmake -- USER_DEFS=-Werror -j3
+cmake --build . -- USER_DEFS=-Werror -j3
 
-cmake --build $PWD/cmake-gdal-debug/apps --target test_ogrsf -- USER_DEFS=-Werror -j3
+cmake --build ./apps --target test_ogrsf -- USER_DEFS=-Werror -j3
 
 sudo rm -f /usr/lib/libgdal.so*
-cmake --target install
+cmake --build . --target install
 sudo ldconfig
 
 cd ../autotest/cpp && make -j3
