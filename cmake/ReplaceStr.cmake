79e1dff80d2d33ae6276322fc02b770ed56a0c14
Hiroshi Miura <miurahr@linux.com>
2017-10-08 17:16:53 +0900

    CMake: Doxygen(WIP)
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- cmake/ReplaceStr.cmake
+++ cmake/ReplaceStr.cmake
@@ -1,3 +1,3 @@
 FILE(READ ${infile} f0)
-STRING( REGEX_REPLACE "${from}" "${to}" f1 "${f0}")
+STRING( REGEX REPLACE "${from}" "${to}" f1 "${f0}")
 FILE(WRITE ${outfile} "${f1}")
--- gdal/ci/travis/cmake/script.sh
+++ gdal/ci/travis/cmake/script.sh
@@ -8,8 +8,6 @@ cd cmake-gdal-debug
 cd autotest/cpp
 # quick test
 ctest -I 1,2,3,4,5,6,7,8,9
-# Compile and test vsipreload
-#make vsipreload.so"
 
 # Run ogr_fgdb test in isolation due to likely conflict with libxml2
 cd ../ogr
@@ -18,5 +16,4 @@ rm ogr_fgdb.py
 cd ../
 
 # Run all the Python autotests
-#python run_all.py"
 ctest -I 1,2,3,4,5,6,7
